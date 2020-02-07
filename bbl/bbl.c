1bff1357af44bb92901e95982d340a8ee90d4c61
James Clarke <jrtc27@jrtc27.com>
2020-02-07 10:44:45 +0000

    Revert "Work around broken IO capability reads on Flute"
    This reverts commit 7f0ed5baa78166d84d352c279422cd8e03b645e8.

--- bbl/bbl.c
+++ bbl/bbl.c
@@ -45,10 +45,7 @@ static void filter_dtb(uintptr_t source)
 {
   uintptr_t dest = dtb_output();
   uint32_t size = fdt_size(source);
-  //memcpy((void*)dest, (void*)source, size);
-  // XXX: Hack to work around IO capability reads clearing high word
-  for (int i = 0; i < size; ++i)
-    ((char *)dest)[i] = ((char *)source)[i];
+  memcpy((void*)dest, (void*)source, size);
 
   // Remove information from the chained FDT
   filter_harts(dest, &disabled_hart_mask);
